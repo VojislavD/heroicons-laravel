048d6c2db55520f2048aef1fc2e6b2ae5af679db
vojislavd <vojislavdragicevic17@gmail.com>
2024-08-09 12:23:28 +0200

    Prepare class to handle icons

---
+++ resources/views/components/icon.blade.php
@@ -0,0 +1,13 @@
+<svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    viewBox="0 0 24 24" 
+    {{ $attributes->merge([
+        'fill' => 'none',
+        'stroke-width' => '1.5',
+        'stroke' => 'currentColor',
+        'class' => 'size-64',
+    ]) }}
+>
+    {!! $path !!}
+</svg>
+  
--- src/View/Components/Icon.php
+++ src/View/Components/Icon.php
@@ -2,13 +2,21 @@
 
 namespace Heroicons\View\Components;
 
+use Heroicons\Icons;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\View\Component;
 
 class Icon extends Component
 {
+    public function __construct(
+        private Icons $icons,
+        private string $name,
+    ) { }
+
     public function render(): Renderable
     {
-        return view('heroicons::components.icon');
+        return view('heroicons::components.icon', [
+            'path' => $this->icons->get($this->name),
+        ]);
     }
 }
