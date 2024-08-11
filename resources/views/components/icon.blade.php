ae479cbfb7af370471134f122f30f0b9162c8c8c
vojislavd <vojislavdragicevic17@gmail.com>
2024-08-11 21:10:30 +0200

    Move attributes to component class

--- resources/views/components/icon.blade.php
+++ resources/views/components/icon.blade.php
@@ -1,12 +1,7 @@
 <svg 
     xmlns="http://www.w3.org/2000/svg" 
     viewBox="0 0 24 24" 
-    {{ $attributes->merge([
-        'fill' => 'none',
-        'stroke-width' => '1.5',
-        'stroke' => 'currentColor',
-        'class' => 'size-64',
-    ]) }}
+    {{ $attributes->merge($iconAttributes) }}
 >
     {!! $path !!}
 </svg>
--- src/View/Components/Icon.php
+++ src/View/Components/Icon.php
@@ -12,12 +12,23 @@ class Icon extends Component
         private Icons $icons,
         private string $name,
         private string $type = 'outline',
-    ) { }
+        private array $iconAttributes = [],
+    ) { 
+        $this->iconAttributes = match ($this->type) {
+            'outline' => [
+                'fill' => 'none',
+                'stroke-width' => '1.5',
+                'stroke' => 'currentColor',
+                'class' => 'size-64',
+            ],
+        };
+    }
 
     public function render(): Renderable
     {
         return view('heroicons::components.icon', [
             'path' => $this->icons->get($this->type, $this->name),
+            'iconAttributes' => $this->iconAttributes,
         ]);
     }
 }
